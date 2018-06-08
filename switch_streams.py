6310652ca837fe68ca75e119e17f15c9e67b38f7
Sightline <sightline.networks@gmail.com>
2018-06-08 00:28:29 -0500

    auto unmute/mute

--- switch_streams.py
+++ switch_streams.py
@@ -20,12 +20,37 @@ if __name__ == '__main__':
         # and switch to the first non-default sink
         for index in args.default_sink_toggle:
 
+
             default_index = int(pulse.get_sink_by_name(pulse.server_info().default_sink_name).index)
 
 
+            default_sink  = pulse.get_sink_by_name(pulse.server_info().default_sink_name)
+
+
+            default_sink.mute = True
+            #current_volume = default_sink.volume
+
+            #current_volume.value_flat = 0.0
+            
+            
+            #pulse.volume_set(default_sink, current_volume)
+
+
+
+            #pulse.sink_input_volume_set(default_sink, current_volume)
+
             
             if int(index) != default_index:
+                # Set the new default and mute the old sink
                 pulse.sink_default_set(index)
+                pulse.mute(default_sink, True)
+               
+                # Unmute the new sink
+                new_sink  = pulse.get_sink_by_name(pulse.server_info().default_sink_name)
+                pulse.mute(new_sink, False)
+
+
+
                 print('default sink set to index: %s' % (index))
                 break
 
